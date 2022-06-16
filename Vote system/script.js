0ee94b48c182d1917943e858aa009da86788da1d
Femke <femke@zoning-out.nl>
2022-06-16 14:31:29 +0200

    no message

--- Vote system/script.js
+++ Vote system/script.js
@@ -1,8 +1,9 @@
-var illustrations = [
-    { name: "Light", votes: 0 },
-    { name: "Heal", votes: 0 },
-    { name: "Ivy", votes: 0},
-    { name: "Mermaid", votes: 0 }
+/* JS FILE */
+var cats = [
+    { name: "Bleu russe", votes: 0 },
+    { name: "Somali", votes: 0 },
+    { name: "egyptian mau", votes: 0},
+    { name: "snowshoe", votes: 0 }
   ]
   
   
@@ -10,24 +11,24 @@ var illustrations = [
   $(function () {
     'use strict';
   
-    $('.illustration-1 .overlay').on('click', function () {
-      illustrations[0].votes += 1;
-      $(this).parent().find('.votes').text(illustrations[0].votes);
+    $('.cat-1 .overlay').on('click', function () {
+      cats[0].votes += 1;
+      $(this).parent().find('.votes').text(cats[0].votes);
     });
   
-    $('.illustration-2 .overlay').on('click', function () {
-      illustrations[1].votes += 1;
-      $(this).parent().find('.votes').text(illustrations[1].votes);
+    $('.cat-2 .overlay').on('click', function () {
+      cats[1].votes += 1;
+      $(this).parent().find('.votes').text(cats[1].votes);
     });
   
-    $('.illustration-3 .overlay').on('click', function () {
-      illustrations[2].votes += 1;
-      $(this).parent().find('.votes').text(illustrations[2].votes);
+    $('.cat-3 .overlay').on('click', function () {
+      cats[2].votes += 1;
+      $(this).parent().find('.votes').text(cats[2].votes);
     });
   
-    $('.illustration-4 .overlay').on('click', function () {
-      illustrations[3].votes += 1;
-      $(this).parent().find('.votes').text(illustrations[3].votes);
+    $('.cat-4 .overlay').on('click', function () {
+      cats[3].votes += 1;
+      $(this).parent().find('.votes').text(cats[3].votes);
     });
   
     $('.All-cats li').on('click', function () {
