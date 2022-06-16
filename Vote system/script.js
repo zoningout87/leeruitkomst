6c35d662930e6dc09db76b30b9e6019adc4531e4
Femke <femke@zoning-out.nl>
2022-06-16 13:55:06 +0200

    Eigen illustraties erin gezet voor het vote systeem.

--- Vote system/script.js
+++ Vote system/script.js
@@ -1,8 +1,8 @@
 var cats = [
-    { name: "Bleu russe", votes: 0 },
-    { name: "Somali", votes: 0 },
-    { name: "egyptian mau", votes: 0},
-    { name: "snowshoe", votes: 0 }
+    { name: "Light", votes: 0 },
+    { name: "Heal", votes: 0 },
+    { name: "Ivy", votes: 0},
+    { name: "Mermaid", votes: 0 }
   ]
   
   
@@ -10,22 +10,22 @@ var cats = [
   $(function () {
     'use strict';
   
-    $('.cat-1 .overlay').on('click', function () {
+    $('.illustration-1 .overlay').on('click', function () {
       cats[0].votes += 1;
       $(this).parent().find('.votes').text(cats[0].votes);
     });
   
-    $('.cat-2 .overlay').on('click', function () {
+    $('.illustration-2 .overlay').on('click', function () {
       cats[1].votes += 1;
       $(this).parent().find('.votes').text(cats[1].votes);
     });
   
-    $('.cat-3 .overlay').on('click', function () {
+    $('.illustration-3 .overlay').on('click', function () {
       cats[2].votes += 1;
       $(this).parent().find('.votes').text(cats[2].votes);
     });
   
-    $('.cat-4 .overlay').on('click', function () {
+    $('.illustration-4 .overlay').on('click', function () {
       cats[3].votes += 1;
       $(this).parent().find('.votes').text(cats[3].votes);
     });
